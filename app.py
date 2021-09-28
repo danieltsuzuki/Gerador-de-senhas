acb8f2b2421258459d933523e92f87bf126373f7
Daniel Tsuzuki <massao.tsuzuki@gmail.com>
2021-09-28 09:33:25 -0300

    commit inicial

--- app.py
+++ app.py
@@ -1,7 +1,86 @@
 import random
-import re
+#import re
+import string
+from tkinter import IntVar
+from tkinter.constants import NUMERIC
+from tkinter import *
+from tkinter import ttk
 
 
+class Gerador_de_senhas:
+    
+    def variaveis(self):
+        self.servico = self.entry_servico.get()
+        self.login = self.entry_login.get()
+        self.tamanho = self.entry_tamanho.get()
+        
+            
+    def show(self):#por enquanto está mostrando o que está recebendo os checkbuttons
+        self.get_checkbuttons()
+        self.compara_checkbuttons()
+        self.gerar_senha()
+        self.get = Label(self.frame, text = ('Senha:',self.senha), bg = '#820AD1', font = ('Arial',15, 'bold'), foreground = '#222222')
+        self.get.place(relx = 0, rely = 0.65, relheight= 0.2, relwidth= 1)
+        
+    
+    def get_checkbuttons(self):#Adiciona o valor 1 na lista para cada opção marcada no CHECKBUTTON e retorna a lista
+        #VARIAVEIS USADAS NOS CHECKBUTTONS {self.numeros, self.especiais, self.minusculos, self.maiusculos}
+        #self.numeros.get() captura o que for digitado pelo usuário
+        self.get_lista = []
+        if(self.numeros.get() == 1):
+            self.get_lista.append(1)
+        else:
+            self.get_lista.append(0)
+        if(self.especiais.get() == 1):
+            self.get_lista.append(1)
+        else:
+            self.get_lista.append(0)
+        if(self.minusculos.get() == 1):
+            self.get_lista.append(1)
+        else:
+            self.get_lista.append(0)
+        if(self.maiusculos.get() == 1):
+            self.get_lista.append(1)
+        else:
+            self.get_lista.append(0)
+        return self.get_lista
+        
+    
+    def compara_checkbuttons(self):#compara as opções CHECKBUTTON com os tipos de caracteres e retorna uma nova lista apenas com os caracteres selecionados
+        self.get_checkbuttons()
+        self.caracteres = []
+        caracter_numericos = string.digits
+        caracter_especiais = string.punctuation
+        caracter_minusculas = string.ascii_lowercase
+        caracter_maiusculas = string.ascii_uppercase
+        tipos_caracter = [caracter_numericos, caracter_especiais, caracter_minusculas, caracter_maiusculas]
+
+        for i in range(0,len(self.get_lista)):
+            if self.get_lista[i]==1:
+                self.caracteres.append(tipos_caracter[i])
+        self.caracteres = ''.join(self.caracteres)#concatena todos os tipos de caracteres selecionados em uma string
+        return self.caracteres
+        
+
+    def gerar_senha(self):
+        self.senha = []
+        self.variaveis()
+        for i in range(int(self.tamanho)):
+            keygen = random.choice(self.caracteres)#keygen recebe os tipos de caracteres que sera escolhido randomicamente
+            self.senha.append(keygen)#adiciona um caractere a lista
+        self.senha = "".join(self.senha)#trasnforma a lista em string, unindo todos os caracteres da senha
+        print(self.senha)
+        return self.senha
+
+
+class Armazenar_senha:
+    
+    def salvar_senha(self):
+        pass
+
+#a = Gerador_de_senhas()
+#a.compara_checkbuttons()
+"""
 caracter = [
     'a','A','b','B','c','C','d','D','e','E','f','F',
     'g','G','h','H','i','I','j','J','l','L','m','M','n','N','o','O','p','P','q','Q','r','R',
@@ -33,12 +112,4 @@ senha = str(senha).strip('[]').replace("'",'').replace(',','').replace(' ','')
 print(type(senha))
 print(senha)
 
-
-
-
-
-
-
-
-
-
+"""
--- gui.py
+++ gui.py
@@ -1,20 +1,87 @@
-from tkinter import *
-from tkinter import ttk
+from app import *
 
 root = Tk()
 
-class Windows():
+class Windows(Gerador_de_senhas):
     def __init__(self):
         self.root = root
         self.tela()
+        self.frame()
+        self.widgets_frame()
         root.mainloop()
+        
     
     def tela(self):
         self.root.title('Gerador de Senhas')#titulo da janela
         self.root.configure(background='black')#cor de fundo da janela
         self.root.geometry('420x360')#o tamanho que a janela se inicia
-        self.root.resizable(True, True)#permite redimensionar a janela nos eixos X e Y
-        self.root.maxsize(width=1920, height=1080)#tamanho maximo da janela
-        self.root.minsize(width=420, height=360)#tamanho minimo da janela
+        self.root.resizable(False, False)#permite redimensionar a janela nos eixos X e Y
+        #self.root.maxsize(width=1920, height=1080)#tamanho maximo da janela
+        #self.root.minsize(width=420, height=360)#tamanho minimo da janela
 
-Windows()
+    def frame(self):
+        self.frame = Frame(self.root, bg = '#820AD1', bd = 2)
+        self.frame.place(relx = 0.01, rely = 0.01, relheight = 0.98, relwidth = 0.98)
+    
+    def widgets_frame(self):
+        #botões###################################################################
+        self.bt_gerar = Button(self.frame, text='Gerar', command = self.show)
+        self.bt_gerar.place(relx = 0.4, rely = 0.85, relheight = 0.1, relwidth = 0.20)
+
+        #label###################################################################
+        self.lb_servico = Label(self.frame, text = 'Serviço', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222')
+        self.lb_servico.place(relx = 0.05, rely = 0.1, relheight = 0.1, relwidth = 0.15)
+
+        self.lb_login = Label(self.frame, text = 'Login', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222')
+        self.lb_login.place(relx = 0.07, rely = 0.2, relheight = 0.1, relwidth = 0.15)
+    
+        self.lb_caracteres = Label(self.frame, text = 'Quais tipos de caracteres terá sua senha?', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222')
+        self.lb_caracteres.place(relx = 0, rely = 0.3, relheight = 0.1, relwidth= 1)
+
+        self.lb_tamanho = Label(self.frame, text = 'Tamanho', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222')
+        self.lb_tamanho.place(relx = 0.65, rely = 0.4, relheight = 0.1, relwidth = 0.2)
+
+        #self.senha = Label(self.frame, text = 'Tamanho', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222')
+        #self.senha.place(relx = 0.0, rely = 0.8, relheight = 0.1, relwidth = 0.2)
+
+        #entry###################################################################
+        self.entry_servico = Entry(self.frame)
+        self.entry_servico.place(relx = 0.21, rely = 0.1, relheight = 0.07,relwidth = 0.75)
+
+        self.entry_login = Entry(self.frame)
+        self.entry_login.place(relx = 0.21, rely = 0.2, relheight = 0.07, relwidth = 0.75)
+
+        self.entry_tamanho = Entry(self.frame)
+        self.entry_tamanho.place(relx = 0.85, rely = 0.4, relheight = 0.07, relwidth = 0.1)
+
+        #self.entry_senha = Entry(self.frame)
+        #self.entry_senha.place(relx = 0.25, rely = 0.8, relheight = 0.1, relwidth = 0.7)
+
+
+        #Check button
+        self.numeros = IntVar()
+        self.especiais = IntVar()
+        self.minusculos = IntVar()
+        self.maiusculos = IntVar()
+        self.op1 = Checkbutton(self.frame, text = 'Números', bg = '#820AD1',font = ('Arial',13, 'bold'), foreground = '#222222', variable = self.numeros)
+        self.op1.place(relx = 0.05, rely = 0.4, relheight = 0.1, relwidth = 0.23)
+        self.op2 = Checkbutton(self.frame, text = 'Especiais', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222', variable = self.especiais)
+        self.op2.place(relx = 0.05, rely = 0.5, relheight = 0.1, relwidth = 0.23)
+        self.op3 = Checkbutton(self.frame, text = 'Minusculos', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222', variable = self.minusculos)
+        self.op3.place(relx = 0.32, rely = 0.4, relheight = 0.1, relwidth = 0.3)
+        self.op4 = Checkbutton(self.frame, text = 'Maiusculos', bg = '#820AD1', font = ('Arial',13, 'bold'), foreground = '#222222', variable = self.maiusculos)
+        self.op4.place(relx = 0.32, rely = 0.5, relheight = 0.1, relwidth = 0.3)
+        
+        #teste
+        #butao = Button(self.frame, text = 'mostrar', command= self.check_button)
+        #butao.place(relx = 0.5, rely = 0.8, relwidth= 0.1, relheight= 0.1)
+
+    #def show(self):
+        #self.get = Label(self.frame, text = self.check_num.get())
+        #self.get.place(relx = 0.5, rely = 0.65, relwidth= 0.1, relheight= 0.1)
+
+
+
+App = Windows()
+#HEIGHT altura
+#WIDTH LARGURA
